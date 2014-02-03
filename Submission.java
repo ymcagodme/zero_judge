bc27c0887f6902ca1bbb3cf5b2b0da6a47aa295e
ymcagodme <ymcagodme@gmail.com>
2014-02-03 12:53:43 -0800

    Init Zero Judge project

---
+++ Submission.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Submission {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println()
+   }
+}
---
+++ isPrime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Submission {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      while(sc.hasNext()) {
+         int num = sc.nextInt();
+         boolean isPrime = true;
+
+         for (int i = 2; i < Math.sqrt(num); ++i) {
+            if (num % i == 0) {
+               isPrime = false;
+            }
+         }
+         if (isPrime)
+            System.out.println("質數");
+         else
+            System.out.println("非質數");
+      }
+   }
+}
+
