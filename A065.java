68fe8194c01c9ce7aebeff32609c0346b356f66c
ymcagodme <ymcagodme@gmail.com>
2014-02-06 11:20:10 -0800

    Submit A065

---
+++ A065.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class A065 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      while (sc.hasNext()) {
+         char[] letters = sc.nextLine().toCharArray();
+         for (char c: letters) {
+            System.out.println((int) c);
+         }
+         for (int i = 0; i < letters.length - 1; ++i) {
+            int distance = Math.abs((int) letters[i + 1] - (int) letters[i]);
+            System.out.print(distance);
+         }
+         System.out.println();
+      }
+   }
+}
