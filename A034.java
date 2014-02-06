5c3e202c3ab65a16006e19243b7b9107fbee1fe6
ymcagodme <ymcagodme@gmail.com>
2014-02-06 08:23:22 -0800

    merge A034

--- A034.java
+++ A034.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 class A034 {
    public static void main(String[] args) {
-<<<<<<< HEAD
       int count = 0;
       boolean flag = true;
 
@@ -11,19 +10,17 @@ class A034 {
          ++count;
          if (count > 10)
             flag = false;
-=======
-      Scanner sc = new Scanner(System.in);
-      while (sc.hasNext()) {
-         int num = sc.nextInt();
-         String ans = "";
+         Scanner sc = new Scanner(System.in);
+         while (sc.hasNext()) {
+            int num = sc.nextInt();
+            String ans = "";
 
-         while ( num != 0 ) {
-            ans = num % 2 + ans;
-            num /= 2;
-         }
-
+            while ( num != 0 ) {
+               ans = num % 2 + ans;
+               num /= 2;
+            }
          System.out.println(ans);
->>>>>>> 68ae127a06dd7bc9d48cd53aa4b0dc297b66f167
+         }
       }
    }
 }
